5fbf0b5bbdac10a8adf67a12e6b4fedc31b9b829
AlexTurmanidze <102172391+AlexTurmanidze@users.noreply.github.com>
2022-06-07 18:00:37 +0300

    Add files via upload

---
+++ Task_SpringHelloWorld/src/main/java/taskspring/App.java
@@ -0,0 +1,23 @@
+package taskspring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        
+        HelloWorld helloWorldBean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorldBean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        
+        System.out.println("helloWorldBean = " + (helloWorldBean == helloWorldBean1));
+        System.out.println("catBean = " + (catBean == catBean1));
+        
+        System.out.println(helloWorldBean.getMessage());
+        System.out.println(catBean.getMessage());
+    }
+}
