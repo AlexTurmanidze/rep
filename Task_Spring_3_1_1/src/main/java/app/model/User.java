0600d112133d9075bca40dbcd05509986ff37a21
AlexTurmanidze <102172391+AlexTurmanidze@users.noreply.github.com>
2022-06-28 23:16:31 +0300

    Add files via upload

---
+++ Task_Spring_3_1_1/src/main/java/app/model/User.java
@@ -0,0 +1,63 @@
+package app.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "firstName")
+    private String firstName;
+    @Column(name = "lastname")
+    private String lastName;
+    @Column(name = "email", unique = true)
+    private String email;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User firstName = " + firstName + ", lastName = " + lastName
+                + ", email = " + email;
+    }
+}
