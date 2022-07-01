962037f7574fd7fc3996632fb3b197756e261597
AlexTurmanidze <102172391+AlexTurmanidze@users.noreply.github.com>
2022-07-01 03:22:12 +0300

    Add files via upload

---
+++ Task_Spring_3_1_1/src/main/java/app/controller/UserController.java
@@ -0,0 +1,53 @@
+package app.controller;
+
+import app.service.UserService;
+import app.model.User;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    
+    @GetMapping
+    public String list(ModelMap model) {
+        List<User> listUsers = userService.findAll();
+        model.addAttribute("users", listUsers);
+        return "userslist";
+    }
+    
+    @GetMapping(value = "/update")
+    public String newForm(@RequestParam(value = "id", required = false) String id, ModelMap model) {
+        User user = new User();
+        if (id != null) {
+            long userId = Long.parseLong(id);
+            user = userService.findById(userId).get();
+        }
+        model.addAttribute("user", user);
+        return "usersEdit";
+    }
+    @PostMapping(value = "/update")
+    public String updateForm(@ModelAttribute User user, ModelMap model) {
+        model.addAttribute("user", user);
+        userService.save(user);
+        
+        return "redirect:/users";
+    }
+    @GetMapping(value = "/delete")
+    public String deleteForm(@RequestParam(value = "id", required = false) String id) {
+        if (id != null) {
+            long userId = Long.parseLong(id);
+            userService.deleteById(userId);
+        }
+        return "redirect:/users";
+    }
+}
